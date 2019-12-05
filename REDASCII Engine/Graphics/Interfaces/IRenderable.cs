f007ebed43f609e0ffab52269e5f8726dda20974
Samuele Lorefice <aironenerowork@gmail.com>
2019-12-05 01:40:22 +0100

    Working day save.

--- REDASCII Engine/Graphics/Interfaces/IRenderable.cs
+++ REDASCII Engine/Graphics/Interfaces/IRenderable.cs
@@ -6,5 +6,6 @@ namespace REDASCII_Engine.Graphics {
     public interface IRenderable {
         public Transform Transform { get; set; }
         public AsciiPixel[,] Data { get; set; }
+
     }
 }
--- REDASCII Engine/Graphics/Renderer.cs
+++ REDASCII Engine/Graphics/Renderer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace REDASCII_Engine.Graphics {
@@ -18,8 +20,76 @@ namespace REDASCII_Engine.Graphics {
             RenderObjects = new List<IRenderable>();
         }
 
+
+        //interop type
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SMALL_RECT {
+            public UInt16 Left;
+            public UInt16 Top;
+            public UInt16 Right;
+            public UInt16 Bottom;
+        }
+
+        //interop type
+        [StructLayout(LayoutKind.Sequential)]
+        public struct COORD {
+            public UInt16 X;
+            public UInt16 Y;
+        }
+
+        [DllImport("Kernel32.dll", EntryPoint = "WriteConsoleOutputW")]
+        private static extern void WriteBufferOutput(IntPtr consoleOutput, char[,] data, COORD bufferSize, COORD startCoord, out SMALL_RECT WriteRegion);
+
         public void DrawBuffer(int bufferIndex) {
+            var bufferPos = Buffers[bufferIndex].BufferPosition;//cache of the buffer position
+            //creates a cache of the buffer that needs to be written
+            AsciiPixel[,] tempBuffer = new AsciiPixel[Buffers[0].Data.GetLength(0), Buffers[0].Data.GetLength(1)];
+
+            RenderObjects.Sort((x, y) => y.Transform.Z.CompareTo(x.Transform.Z));//sorting the list to the Z in descending order
+            foreach (IRenderable renderable in RenderObjects) {
+                Transform localPosition = renderable.Transform;
+                for (int x = 0; x < renderable.Data.GetLength(1); x++) {//each line
+                    for (int y = 0; y < renderable.Data.GetLength(0); y++) {
+                        if (renderable.Data[x, y].Data != '\0') {
+                            tempBuffer[x + localPosition.X, y + localPosition.Y].Data = renderable.Data[x, y].Data;
+                            tempBuffer[x + localPosition.X, y + localPosition.Y].Color = renderable.Data[x, y].Color;
+                        }
+                    }
+                }
+            }
+            Buffers[bufferIndex].Data = tempBuffer;
+            //separating colors
+            var renderPasses = new Dictionary<ConsoleColor, char[,]>();
+            for (int x = 0; x < tempBuffer.GetLength(0); x++) {
+                for (int y = 0; y < tempBuffer.GetLength(1); y++) {
+                    if(renderPasses.ContainsKey(tempBuffer[x, y].Color)){//check if the color of the pixel already have a buffer
+                        renderPasses[tempBuffer[x, y].Color][x, y] = tempBuffer[x, y].Data;//add the pixel at the right position
+                    } else {
+                        renderPasses.Add(tempBuffer[x, y].Color, new char[tempBuffer.GetLength(0), tempBuffer.GetLength(1)]);//add the new buffer
+                        renderPasses[tempBuffer[x, y].Color][x, y] = tempBuffer[x, y].Data;//then add the pixel
+                    }
+                }
+            }
+            //gets the console handle
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+
+            //https://docs.microsoft.com/en-us/windows/console/writeconsoleoutput
             
+            //finally write the buffer, one color at the time:
+            foreach (var colorBuffer in renderPasses) {
+                WriteBufferOutput(handle, colorBuffer.Value, new COORD() { });
+                //Console.ForegroundColor = colorBuffer.Key;
+                //for (int y = 0; y < colorBuffer.Value.GetLength(1); y++) {
+                //    string line = "";
+                //    for (int x = 0; x < colorBuffer.Value.GetLength(0); x++) {
+                //        if (colorBuffer.Value[x, y] == '\0') {
+                //            line += " ";
+                //        } else {
+                //            line += colorBuffer.Value[x, y];
+                //        }
+                //    }
+                //}
+            }
         }
 
         public void Run() {
@@ -44,6 +114,7 @@ namespace REDASCII_Engine.Graphics {
                 Console.WindowLeft,
                 Console.WindowTop
             );
+
         }
     }
 }
