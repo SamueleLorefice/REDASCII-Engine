923fd466a1593c54fbae9606a7a765186761d844
Samuele Lorefice <aironenerowork@gmail.com>
2019-12-05 19:55:55 +0100

    Added bindings to AdvancedConsole class (old Console.cs class)

---
+++ AdvancedConsole/AdvancedConsole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AdvancedConsole {
+    public class AdvancedConsole {
+        [DllImport("Kernel32.dll", EntryPoint = "WriteConsoleOutputW")]
+        public static extern void WriteBufferOutput(IntPtr consoleOutput, char[,] data, COORD bufferSize, COORD startCoord, out SMALL_RECT WriteRegion);
+
+        [DllImport("Kernel32.dll", EntryPoint = "ReadConsole")]
+        public static extern void ReadConsole();
+    }
+}
--- AdvancedConsole/Console.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Runtime.InteropServices;
-
-namespace AdvancedConsole {
-    public class Console {
-        
-    }
-}
